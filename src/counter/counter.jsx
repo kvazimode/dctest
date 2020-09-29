569cb41694d18b9cdfa1f3be308a9a7a5415aab9
kvazi <kvazimode2@gmail.com>
2020-09-29 18:37:09 +0300

    description block added

--- src/counter/counter.jsx
+++ src/counter/counter.jsx
@@ -8,10 +8,16 @@ const Counter = (props) => {
   }
   
   return <React.Fragment>
-    <label htmlFor="input" className="input-label" title="set me">set me</label><br/>
-    <input type="button" className="decrease" value="decrease" onClick={() => onValueChange(value - 1)}/>
-    <input type="number" className="input" id="input" value={value} min={min} max={max} onChange={() => onValueChange(document.querySelector(`.input`).value)} onWheel={(evt) => wheelHandler(evt, document.querySelector(`.input`).value)}/>
-    <input type="button" className="increase" value="increase" onClick={() => onValueChange(value + 1)}/>
+    <div className="description">
+      <p>Текущее минимальное значение: {min}</p>
+      <p>Текущее максимальное значение: {max}</p>
+    </div>
+    <div className="counter">
+      <label htmlFor="input" className="input-label" title="set me">set me</label><br/>
+      <input type="button" className="decrease" value=" - " onClick={() => onValueChange(value - 1)}/>
+      <input type="number" className="input" id="input" value={value} min={min} max={max} onChange={() => onValueChange(document.querySelector(`.input`).value)} onWheel={(evt) => wheelHandler(evt, document.querySelector(`.input`).value)}/>
+      <input type="button" className="increase" value=" + " onClick={() => onValueChange(value + 1)}/>
+    </div>
   </React.Fragment>
 }
 
