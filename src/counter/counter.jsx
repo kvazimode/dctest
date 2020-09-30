72de0d29321036fbfe9e3997bf0c3390bbf574d2
kvazi <kvazimode2@gmail.com>
2020-09-30 13:11:27 +0300

    input is ref now

--- src/counter/counter.jsx
+++ src/counter/counter.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 class Counter extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.inputRef = React.createRef();
     this._wheelHandler = this._wheelHandler.bind(this);
   }
 
-  _wheelHandler(evt, val) {
-    evt.deltaY < 0 ? val++ : val--
+  _wheelHandler(evt) {
+    let val = this.inputRef.current.value;
+    evt.deltaY < 0 ? val++ : val--;
     return this.props.onValueChange(val)
   }
   
@@ -21,7 +23,15 @@ class Counter extends React.PureComponent {
     <div className="counter">
       <label htmlFor="input" className="input-label" title="set me">set me</label>
       <input type="button" className="decrease" value=" - " onClick={() => onValueChange(value - 1)}/>
-      <input type="number" className="input" id="input" value={value} min={min} max={max} onChange={() => onValueChange(document.querySelector(`.input`).value)} onWheel={(evt) => this._wheelHandler(evt, document.querySelector(`.input`).value)}/>
+      <input type="number"
+        className="input"
+        id="input"
+        value={value}
+        min={min}
+        max={max}
+        onChange={() => onValueChange(this.inputRef.current.value)}
+        onWheel={(evt) => this._wheelHandler(evt)} ref={this.inputRef}
+      />
       <input type="button" className="increase" value=" + " onClick={() => onValueChange(value + 1)}/>
     </div>
   </React.Fragment>
